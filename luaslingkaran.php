89e9abaeea9209d8780c75d2e4d8a92c189ba7b8
zaidanmubarak <if24.ahmadmubarak@mhs.ubpkarawang.ac.id>
2025-11-14 20:21:11 +0700

    pertemuan 4

--- luaslingkaran.php
+++ luaslingkaran.php
@@ -1,16 +1,9 @@
-
 <?php
-
-namespace App\Math;
 
 class LuasLingkaran {
 
     public const phi = 3.14;
-    protected int $jari;
-    
-    public function __construct($isiJari = 1) {
-        $this->jari = $isiJari; //10
-    }
+    public int $jari;
 
     public function tampil($nama = 'ban') {
         $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
@@ -21,9 +14,10 @@ class LuasLingkaran {
         echo "<br/>";
         echo "ini dari static";
     }
-
-    public function __destruct() {
-        echo "<br/>";
-        echo "udah ah cape";
-    }
 }
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 12;
+$lingkaran->tampil('roda'); //panggil method tampil
+
+LuasLingkaran::testing(); //panggil static method
